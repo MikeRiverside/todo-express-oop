2497d0d22b62c213abe954fd18c7dba5f018c5f1
Mihkel Jõesalu <mihkel.joesalu@voco.ee>
2024-11-29 15:59:06 +0200

    project start files

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ import express from "express"
 import bodyParser from "body-parser"
 
 const app = express ()
-app.use(bodyParser.json())
+
+app.use(express.json())
+
+app.get("/json-test", (req, res) => {
+    res.json({
+        message: "json test ok"
+    })
+})
 
 app.listen(5005, () => {
     console.log("server on ühendatud")
