b793b1355c2e4542334c33bb6f573917d611ed5c
Mihkel Jõesalu <mihkel.joesalu@voco.ee>
2024-11-29 14:56:11 +0200

    create basic project files

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express ()
+app.use(bodyParser.json())
+
+app.listen(5005, () => {
+    console.log("server on ühendatud")
+})
